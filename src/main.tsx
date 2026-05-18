d2023a8169adb3e8801a6f42448a826cbf425e3f
Delemangi <milev.stefan@gmail.com>
2026-05-18 08:27:33 +0200

    Add application entrypoint

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from '@/App';
+import '@/index.css';
+
+const root = document.querySelector('#root');
+
+if (!(root instanceof HTMLElement)) {
+  throw new TypeError('Root element not found');
+}
+
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
