816230e8445a6d44cd2b6e445debb85072d92b9a
Delemangi <milev.stefan@gmail.com>
2026-06-28 18:23:00 +0200

    fix: use identified_only profiles and capture SPA pageviews

--- web/src/lib/analytics.ts
+++ web/src/lib/analytics.ts
@@ -28,7 +28,9 @@ export const initAnalytics = () => {
     // eslint-disable-next-line camelcase -- PostHog option keys are snake_case.
     capture_exceptions: true,
     // eslint-disable-next-line camelcase -- PostHog option keys are snake_case.
-    person_profiles: 'always',
+    capture_pageview: 'history_change',
+    // eslint-disable-next-line camelcase -- PostHog option keys are snake_case.
+    person_profiles: 'identified_only',
   });
 };
 
